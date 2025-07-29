92c5f0a3bd9c245f81a78f0b91f632fd6d36e0cf
saahithy555 <saahithyk@gmail.com>
2025-07-28 22:22:29 -0700

    Added Subtraction feature

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,11 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 4;
         int b=6;
+        int c=20;
+        int d=15;
         int x=a+b;
+        int y=c-d;
         System.out.println("Addition of Numbers: " +x);
+        System.out.println("Subtraction of Numbers: " +y);
     }
 }
